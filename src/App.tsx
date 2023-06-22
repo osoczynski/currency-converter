3f494c5042902565e060145e8eddfddeb3112f4e
osoczynski <o.soczynski@gmail.com>
2023-06-22 16:31:31 +0200

    Styles + bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import "./css/App.css";
 import { useEffect, useState } from "react";
-import "./App.css";
 import CurrencyItem from "./components/CurrencyItem";
-import Dropdown from "react-dropdown";
-import "react-dropdown/style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
+
 const access_key = "bdnxFnsnEkbL0WvLAB2bX7TWc2h7RwxuKxoKlEnd";
 const BASE_URL = "https://api.freecurrencyapi.com/v1/latest?apikey=";
 
@@ -10,22 +11,31 @@ function App() {
   const [currenciesOptions, setCurrenciesOptions] = useState<string[]>([]);
   const [fromCurrency, setFromCurrency] = useState<string>("");
   const [toCurrency, setToCurrency] = useState<string>("");
-  const [amount, setAmount] = useState<number>(1);
+  const [amount, setAmount] = useState<number | string>();
   const [fromAmountCurrency, setFromtAmontCurrency] = useState<boolean>(true);
-  const [exchangeRate, setExchangeRate] = useState<number>(1);
+  const [exchangeRate, setExchangeRate] = useState<number>(0);
 
   let toAmount, fromAmount;
-  if (fromAmountCurrency) {
-    fromAmount = amount;
-    toAmount = amount * exchangeRate;
+  if (amount !== "") {
+    if (fromAmountCurrency) {
+      fromAmount = amount;
+      toAmount = +(+amount! * exchangeRate).toFixed(2);
+    } else {
+      toAmount = amount;
+      fromAmount = +(+amount! / exchangeRate).toFixed(2);
+    }
   } else {
-    toAmount = amount;
-    fromAmount = amount / exchangeRate;
+    toAmount = "";
+    fromAmount = "";
   }
   useEffect(() => {
     fetch(`${BASE_URL}${access_key}`)
       .then((res) => res.json())
-      .then((data) => setCurrenciesOptions(Object.keys(data.data)));
+      .then((data) => {
+        setCurrenciesOptions(Object.keys(data.data));
+        setFromCurrency(Object.keys(data.data)[8]);
+        setToCurrency(Object.keys(data.data)[31]);
+      });
   }, []);
 
   useEffect(() => {
@@ -40,31 +50,43 @@ function App() {
   }, [fromCurrency, toCurrency]);
 
   const handleFromChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAmount(+e.target.value);
-    setFromtAmontCurrency(true);
+    if (e.target.value !== "") {
+      setAmount(+e.target.value);
+      setFromtAmontCurrency(true);
+    } else setAmount("");
   };
 
   const handleToChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAmount(+e.target.value);
-    setFromtAmontCurrency(false);
+    if (e.target.value !== "") {
+      setAmount(+e.target.value);
+      setFromtAmontCurrency(false);
+    } else setAmount("");
+  };
+
+  const handleSwitch = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
   };
 
   return (
     <div className="converter">
-      <h1>Converter</h1>
+      <h1>Currency Converter</h1>
       <CurrencyItem
+        placeHolder="From"
         currencies={currenciesOptions}
         selectedCurrency={fromCurrency}
-        amount={fromAmount}
+        amount={fromAmount!}
         onChangeAmount={handleFromChange}
         onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
           setFromCurrency(e.target.value)
         }
       />
+      <FontAwesomeIcon icon={faRightLeft} onClick={handleSwitch} />
       <CurrencyItem
+        placeHolder="To"
         currencies={currenciesOptions}
         selectedCurrency={toCurrency}
-        amount={toAmount}
+        amount={toAmount!}
         onChangeAmount={handleToChange}
         onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
           setToCurrency(e.target.value)
--- src/components/CurrencyItem.tsx
+++ src/components/CurrencyItem.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import "../css/CurrencyItem.css";
 
 const CurrencyItem: React.FC<{
+  placeHolder: string;
   currencies: string[];
   selectedCurrency: string;
-  amount: number;
+  amount: number | string;
   onChangeAmount: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }> = (props) => {
   return (
-    <div>
+    <div className="item">
       <input
+        placeholder={props.placeHolder}
         type="number"
         value={props.amount}
+        min={1}
         onChange={props.onChangeAmount}
       />
       <select value={props.selectedCurrency} onChange={props.onChange}>
